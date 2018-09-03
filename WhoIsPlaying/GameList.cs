c4a5cad6a6d7826333484e3a7594e941a091b788
Delia Alvarez <dalvarez@targethealth.com>
2018-09-03 12:37:02 -0300

    Game List

--- WhoIsPlaying/GameList.cs
+++ WhoIsPlaying/GameList.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -5,31 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+using WhoIsPlaying.Common;
 
 namespace WhoIsPlaying
 {
     public static class GameList
     {
         [FunctionName("gameList")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]HttpRequestMessage req, TraceWriter log, [Table("game")] CloudTable gamesTable)
         {
-            log.Info("C# HTTP trigger function processed a request.");
-
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
-
-            if (name == null)
-            {
-                // Get request body
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                name = data?.name;
-            }
-
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            log.Info("Getting game list items");
+            var query = new TableQuery<EventTableEntity>();
+            var items = await gamesTable.ExecuteQuerySegmentedAsync(query, null);
+            return req.CreateResponse(HttpStatusCode.OK, items.Select(game => new {
+                EventDateAndTime = game.EventDateAndTime,
+                Location = game.Location,
+                Invitees = JsonConvert.DeserializeObject<Response[]>(game.ResponsesJson)
+            }));
         }
     }
 }
