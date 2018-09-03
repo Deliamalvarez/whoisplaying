ad124dcdd5b8ecf8bcab22f1250f36b8935b7ded
Martin Acosta <macosta@velocitypartners.net>
2018-09-03 12:41:06 -0300

    add get user list

--- WhoIsPlaying/Common/EventDetails.cs
+++ WhoIsPlaying/Common/EventDetails.cs
@@ -1,47 +1,49 @@
-﻿using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table;
+﻿using Microsoft.WindowsAzure.Storage.Table;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WhoIsPlaying.Common
 {
-    public class EventDetails
-    {
-        public DateTime EventDateAndTime { get; set; }
-        public string Location { get; set; }
-        public List<InviteeDetails> Invitees { get; set; }
-    }
+	public class EventDetails
+	{
+		public DateTime EventDateAndTime { get; set; }
+		public string Location { get; set; }
+		public List<InviteeDetails> Invitees { get; set; }
+	}
 
-    public class InviteeDetails
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
+	public class InviteeDetails
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+	}
 
-    public class EmailDetails
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string ResponseUrl { get; set; }
-        public string Location { get; set; }
-        public DateTime EventDateAndTime { get; set; }
-    }
+	public class EmailDetails
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string ResponseUrl { get; set; }
+		public string Location { get; set; }
+		public DateTime EventDateAndTime { get; set; }
+	}
 
-    public class EventTableEntity: TableEntity 
-    {
-        public string ResponsesJson { get; set; }
-        public string Location { get; set; }
-        public DateTime EventDateAndTime { get; set; }
-    }
+	public class EventTableEntity: TableEntity 
+	{
+		public string ResponsesJson { get; set; }
+		public string Location { get; set; }
+		public DateTime EventDateAndTime { get; set; }
+	}
 
-    public class Response
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string IsPlaying { get; set; }
-        public string ResponseCode { get; set; }
-    }
+	public class UserEntity : TableEntity
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+	}
+
+	public class Response
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string IsPlaying { get; set; }
+		public string ResponseCode { get; set; }
+	}
 }
---
+++ WhoIsPlaying/UserList.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage.Table;
+using WhoIsPlaying.Common;
+
+namespace WhoIsPlaying
+{
+	public static class UserList
+	{
+		[FunctionName("userList")]
+		public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route =
+				null)]
+			HttpRequestMessage req, TraceWriter log
+			, [Table("users")] CloudTable usersTable, string id, string responseCode)
+		{
+			log.Info($"Getting users");
+			var query = new TableQuery<UserEntity>();
+			var items = await usersTable.ExecuteQuerySegmentedAsync(query, null);
+			return req.CreateResponse(HttpStatusCode.OK, items.Select(user => new {
+				Name = user.Name,
+				Email = user.Email
+			}));
+		}
+	}
+
+}
