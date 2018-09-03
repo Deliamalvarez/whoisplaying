c59ff6d10ee1130ceb279443d1fb916e0ebd45ab
Martin Acosta <macosta@velocitypartners.net>
2018-09-03 18:16:00 -0300

    crear evento

--- ui/whoisplaingWeb/src/app/shared/components/event-new/event-new.component.ts
+++ ui/whoisplaingWeb/src/app/shared/components/event-new/event-new.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { NewGame } from '../../models/event';
+import { forEach } from '@angular/router/src/utils/collection';
+import { EventService } from '../../services/event.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-event-new',
@@ -8,7 +12,12 @@ import { UserService } from '../../services/user.service';
 })
 export class EventNewComponent implements OnInit {
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private eventService: EventService, private router: Router) { }
+
+  nombre:string = '';
+  email:string = '';
+  location: string = '';
+  fecha:Date;
 
   users:any;
 
@@ -19,7 +28,31 @@ export class EventNewComponent implements OnInit {
   }
 
   addEvent() {
+    let newGame = new NewGame();
+    newGame.Email = this.email;
+
+    if(this.fecha){
+      newGame.EventDateAndTime = new Date(this.fecha);
+    }
+    newGame.Location = this.location;
+    newGame.Name = this.nombre;
+    newGame.ResponseUrl = "click";
+    newGame.Invitees = [];
     
+    this.users.forEach(item => {
+      if (item.Required){
+        newGame.Invitees.push({Name: item.Name, Email: item.Email})
+      }
+    })
+    console.log(newGame);
+    this.eventService.createGame(newGame).subscribe(item =>{
+      console.log(item);
+      if (item) {
+      this.router.navigate(['/events', item.Id]);
+      } else {
+        this.router.navigate(['/events']);
+      }
+    })
   }
 
 }
